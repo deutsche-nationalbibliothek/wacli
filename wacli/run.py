1fc161f678bcd53f02391fc1f37ecfb91815c86f
Natanael Arndt <arndtn@gmail.com>
2024-06-11 14:21:27 +0200

    Move namespace manager and store to common places

--- wacli/run.py
+++ wacli/run.py
@@ -4,6 +4,18 @@ from rdflib import Graph
 from rdflib.namespace import Namespace, NamespaceManager
 from rdflib.plugins.stores.sparqlstore import SPARQLStore
 
+namespaces = NamespaceManager(Graph())
+namespaces.bind("rdf", Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#"))
+namespaces.bind("rdfs", Namespace("http://www.w3.org/2000/01/rdf-schema#"))
+namespaces.bind("foaf", Namespace("http://xmlns.com/foaf/0.1/"))
+namespaces.bind("dc", Namespace("http://purl.org/dc/elements/1.1/"))
+namespaces.bind("dcterms", Namespace("http://purl.org/dc/terms/"))
+namespaces.bind("bibo", Namespace("http://purl.org/ontology/bibo/"))
+namespaces.bind("gndo", Namespace("https://d-nb.info/standards/elementset/gnd#"))
+namespaces.bind("xsd", Namespace("http://www.w3.org/2001/XMLSchema#"))
+namespaces.bind("wdrs", Namespace("http://www.w3.org/2007/05/powder-s#"))
+namespaces.bind("rdact", Namespace("http://rdaregistry.info/termList/RDACarrierType/"))
+
 
 @click.group()
 @click.option("--endpoint", envvar="SPARQL_TITLE_DATA")
@@ -11,13 +23,17 @@ from rdflib.plugins.stores.sparqlstore import SPARQLStore
 def cli(ctx, endpoint):
     ctx.ensure_object(dict)
     ctx.obj["endpoint"] = endpoint
+    # specify format due to https://github.com/ad-freiburg/qlever/issues/1372
+    ctx.obj["store_construct"] = SPARQLStore(
+        query_endpoint=endpoint, returnFormat="turtle"
+    )
+    ctx.obj["store_select"] = SPARQLStore(query_endpoint=endpoint)
 
 
 @cli.command()
 @click.pass_context
 def test_select(ctx):
-    store = SPARQLStore(query_endpoint=ctx.obj["endpoint"])
-    remote_graph = Graph(store=store)
+    remote_graph = Graph(store=ctx.obj["store_select"])
     res = remote_graph.query("""
         select * {
         ?s ?p ?o
@@ -30,9 +46,7 @@ def test_select(ctx):
 @cli.command()
 @click.pass_context
 def test_construct(ctx):
-    # specify format due to https://github.com/ad-freiburg/qlever/issues/1372
-    store = SPARQLStore(query_endpoint=ctx.obj["endpoint"], returnFormat="turtle")
-    remote_graph = Graph(store=store)
+    remote_graph = Graph(store=ctx.obj["store_construct"])
     res = remote_graph.query("""
         construct {
         ?s ?p ?o
@@ -46,22 +60,7 @@ def test_construct(ctx):
 @cli.command()
 @click.pass_context
 def list(ctx):
-    namespaces = NamespaceManager(Graph())
-    namespaces.bind("rdf", Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#"))
-    namespaces.bind("rdfs", Namespace("http://www.w3.org/2000/01/rdf-schema#"))
-    namespaces.bind("foaf", Namespace("http://xmlns.com/foaf/0.1/"))
-    namespaces.bind("dc", Namespace("http://purl.org/dc/elements/1.1/"))
-    namespaces.bind("dcterms", Namespace("http://purl.org/dc/terms/"))
-    namespaces.bind("bibo", Namespace("http://purl.org/ontology/bibo/"))
-    namespaces.bind("gndo", Namespace("https://d-nb.info/standards/elementset/gnd#"))
-    namespaces.bind("xsd", Namespace("http://www.w3.org/2001/XMLSchema#"))
-    namespaces.bind("wdrs", Namespace("http://www.w3.org/2007/05/powder-s#"))
-    namespaces.bind(
-        "rdact", Namespace("http://rdaregistry.info/termList/RDACarrierType/")
-    )
-
-    store = SPARQLStore(query_endpoint=ctx.obj["endpoint"], returnFormat="turtle")
-    remote_graph = Graph(store=store, namespace_manager=namespaces)
+    remote_graph = Graph(store=ctx.obj["store_construct"], namespace_manager=namespaces)
     remote_result = remote_graph.query("""
         CONSTRUCT {
           ?page foaf:isPrimaryTopicOf ?topic;
