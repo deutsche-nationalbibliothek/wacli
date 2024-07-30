5ef530b079726f6b1582f9588dad89de3fac87a1
Natanael Arndt <arndtn@gmail.com>
2024-07-30 14:07:32 +0200

    Adjust directory storage to new interface

--- wacli_plugins/storage/directory.py
+++ wacli_plugins/storage/directory.py
@@ -1,5 +1,6 @@
 """This is the directory storage module."""
 
+from collections.abc import Callable
 from io import BytesIO, StringIO
 from os import listdir
 from os.path import isdir, isfile
@@ -17,54 +18,55 @@ class DirectoryStorage(StoragePlugin):
     def configure(self, configuration):
         self.path = Path(configuration.get("path"))
 
-    def get_stream(
+    def store(
+        self,
+        id: str,
+        data: Union[TextIO, BinaryIO, Callable],
+        metadata: {},
+    ):
+        """Create a file with the given id as name in the directory."""
+
+        mode = "w"
+        if isinstance(data, BytesIO):
+            mode = "wb"
+
+        with self.retrieve(id, mode) as target:
+            if isinstance(data, StringIO) or isinstance(data, BytesIO):
+                logger.debug("Write from stream")
+                target.write(data.read())
+
+    def store_stream(
+        self,
+        stream: list,
+    ):
+        """Write the stream to the directory."""
+        for id, data, metadata in stream:
+            self.store()
+
+    def retrieve(
+        self,
+        id: str,
+        mode: str = "r",
+    ) -> list[tuple[str, Union[TextIO, BinaryIO, Callable], dict]]:
+        if mode not in ["r", "w", "rb", "wb"]:
+            raise Exception("Only 'r', 'w', 'rb', and 'wb' modes are supported.")
+
+        return self._retrieve(self.path, [id], mode)
+
+    def retrieve_stream(
         self,
         selector,
-        mode: str = "w",
-    ):
+        mode: str = "r",
+    ) -> list[tuple[str, Union[TextIO, BinaryIO, Callable], dict]]:
         """Create a file with the given id as name in the directory."""
 
         if mode not in ["r", "w", "rb", "wb"]:
             raise Exception("Only 'r', 'w', 'rb', and 'wb' modes are supported.")
 
-        if selector is None:
+        if not selector:
             raise Exception("DirectoryStorage needs a list of explicite IDs")
 
-        for id in selector:
-            yield id, self.retrieve(id, mode)
-
-    def store(
-        self,
-        id: str,
-        data: Union[TextIO, BinaryIO, None] = None,
-        mode: str = "w",
-    ):
-        """Create a file with the given id as name in the directory."""
-
-        if mode not in ["w", "wb"]:
-            raise Exception("Only 'w' and 'wb' modes are supported.")
-
-        if isinstance(data, BytesIO) or isinstance(data, bytes):
-            if len(mode) == 1:
-                mode += "b"
-
-        with self.get_stream(id, mode) as target:
-            if isinstance(data, StringIO) or isinstance(data, BytesIO):
-                logger.debug("Write from stream")
-                target.write(data.read())
-            else:
-                logger.debug("Write data")
-                target.write(data)
-
-    def retrieve(
-        self,
-        id,
-        mode: str = "r",
-    ):
-        if mode not in ["r", "rb"]:
-            raise Exception("Only 'r' and 'rb' modes are supported.")
-
-        return self._retrieve(self.path, id, mode)
+        return self._retrieve(self.path, selector, mode)
 
     def list(self) -> list:
         return [d for d in listdir(self.path) if isdir(self.path / d)]
@@ -72,17 +74,18 @@ class DirectoryStorage(StoragePlugin):
     def _retrieve(
         self,
         path,
-        id,
+        selector,
         mode: str = "r",
     ):
-        if mode not in ["r", "rb"]:
-            raise Exception("Only 'r' and 'rb' modes are supported.")
+        #  -> list[tuple[str, Union[TextIO, BinaryIO, Callable], dict]]
+        if mode not in ["r", "w", "rb", "wb"]:
+            raise Exception("Only 'r', 'w', 'rb', and 'wb' modes are supported.")
 
-        if isfile(path / id):
-            yield id, lambda: open(path / id, mode)
-        else:
-            for name in listdir(path / id):
-                yield name, self._retrieve(path / id, name, mode)
+        for id in selector:
+            if isfile(path / id):
+                yield id, lambda: open(path / id, mode), {}
+            else:
+                yield id, self._retrieve(path / id, listdir(path / id), mode), {}
 
     def list(self) -> list:
         return [d for d in listdir(self.path) if isdir(self.path / d)]
