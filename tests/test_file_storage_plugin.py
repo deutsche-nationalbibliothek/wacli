f92db40358313bae13412555311a59836da4b022
Natanael Arndt <arndtn@gmail.com>
2024-07-30 16:04:47 +0200

    Fix file storea tests

--- tests/test_file_storage_plugin.py
+++ tests/test_file_storage_plugin.py
@@ -15,38 +15,6 @@ def get_plugin_config(path):
         ]
     }
 
-
-def test_store_string(tmp_path):
-    plugin_manager = PluginManager()
-    p = tmp_path / "example_file.txt"
-    plugin_manager.register_plugins(get_plugin_config(p))
-
-    test_storage = plugin_manager.get("test_storage")
-    content = "Hallo"
-
-    test_storage.store("", content)
-
-    assert p.read_text() == content
-    assert len(list(tmp_path.iterdir())) == 1
-
-
-def test_store_bytes(tmp_path):
-    plugin_manager = PluginManager()
-    p = tmp_path / "example_file.txt"
-    plugin_manager.register_plugins(get_plugin_config(p))
-
-    test_storage = plugin_manager.get("test_storage")
-
-    content = "Hallo binary"
-    binary_content = content.encode("utf-8")
-    logger.debug(binary_content)
-
-    test_storage.store("", binary_content)
-
-    assert p.read_text() == content
-    assert len(list(tmp_path.iterdir())) == 1
-
-
 def test_store_text_io(tmp_path):
     plugin_manager = PluginManager()
     p = tmp_path / "example_file.txt"
@@ -56,7 +24,7 @@ def test_store_text_io(tmp_path):
     content = "some initial text data"
     stream = StringIO(content)
 
-    test_storage.store("", stream)
+    test_storage.store("", lambda: stream)
 
     assert p.read_text() == content
     assert len(list(tmp_path.iterdir())) == 1
@@ -72,13 +40,13 @@ def test_store_binary_io(tmp_path):
     binary_content = content.encode("utf-8")
     stream = BytesIO(binary_content)
 
-    test_storage.store("", stream)
+    test_storage.store("", lambda: stream)
 
     assert p.read_text() == content
     assert len(list(tmp_path.iterdir())) == 1
 
 
-def test_get_stream_text_io(tmp_path):
+def test_retrieve_write_text_io(tmp_path):
     plugin_manager = PluginManager()
     p = tmp_path / "example_file.txt"
     plugin_manager.register_plugins(get_plugin_config(p))
@@ -86,14 +54,14 @@ def test_get_stream_text_io(tmp_path):
     test_storage = plugin_manager.get("test_storage")
     content = "Hallo"
 
-    with test_storage.get_stream("") as stream:
+    with test_storage.retrieve("", mode="w")[0]() as stream:
         stream.write(content)
 
     assert p.read_text() == content
     assert len(list(tmp_path.iterdir())) == 1
 
 
-def test_get_stream_binary_io(tmp_path):
+def test_retrieve_write_binary_io(tmp_path):
     plugin_manager = PluginManager()
     p = tmp_path / "example_file.txt"
     plugin_manager.register_plugins(get_plugin_config(p))
@@ -104,14 +72,14 @@ def test_get_stream_binary_io(tmp_path):
     binary_content = content.encode("utf-8")
     logger.debug(binary_content)
 
-    with test_storage.get_stream("", mode="wb") as stream:
+    with test_storage.retrieve("", mode="wb")[0]() as stream:
         stream.write(binary_content)
 
     assert p.read_text() == content
     assert len(list(tmp_path.iterdir())) == 1
 
 
-def test_retrieve_str(tmp_path):
+def test_retrieve_read_str(tmp_path):
     plugin_manager = PluginManager()
     p = tmp_path / "example_file.txt"
     plugin_manager.register_plugins(get_plugin_config(p))
@@ -122,6 +90,6 @@ def test_retrieve_str(tmp_path):
     with open(p, "w") as fp:
         fp.write(content)
 
-    result_content = test_storage.retrieve("")
+    result_content = test_storage.retrieve("")[0]().read()
 
     assert result_content == content
