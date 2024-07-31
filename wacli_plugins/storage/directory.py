51f3e2485f93085a179db516a0ce0a65abd29527
Natanael Arndt <arndtn@gmail.com>
2024-07-31 17:09:44 +0200

    Fix directory storage to create parent directories and handle byte, string, and incompatible streams

--- wacli_plugins/storage/directory.py
+++ wacli_plugins/storage/directory.py
@@ -1,11 +1,13 @@
 """This is the directory storage module."""
 
 from collections.abc import Callable
-from io import DEFAULT_BUFFER_SIZE, BytesIO
+from io import DEFAULT_BUFFER_SIZE, TextIOBase
 from os import listdir
 from os.path import exists, isdir, isfile
 from pathlib import Path
-from typing import BinaryIO, TextIO, Union
+from typing import IO, Union
+
+from loguru import logger
 
 from wacli.plugin_types import StoragePlugin
 
@@ -19,7 +21,7 @@ class DirectoryStorage(StoragePlugin):
     def store(
         self,
         id: str,
-        data: Callable[[], Union[TextIO, BinaryIO]],
+        data: Callable[[], IO],
         metadata: dict = {},
     ):
         """Create a file with the given id as name in the directory."""
@@ -27,50 +29,60 @@ class DirectoryStorage(StoragePlugin):
 
     def _store_data(self, path, data, metadata):
         with data() as source_io:
-            mode = "w"
-            if isinstance(source_io, BytesIO):
-                mode = "wb"
+            if isinstance(source_io, TextIOBase):
+                mode = "w"
+            else:
+                mode = getattr(source_io, "mode", "wb")
 
             target, target_metadata = self._retrieve(path, mode)
             with target() as target_io:
-                while True:
-                    chunk = source_io.read(DEFAULT_BUFFER_SIZE)
-                    if chunk:
+                source_io.wacli_read = source_io.read
+                while chunk := source_io.wacli_read(DEFAULT_BUFFER_SIZE):
+                    try:
+                        logger.debug("write")
                         target_io.write(chunk)
-                    else:
-                        break
+                    except TypeError:
+                        logger.debug("overwrite source_io.wacli_read")
+                        target_io.write(chunk.encode("utf-8"))
+                        source_io.wacli_read = lambda buffer_size: source_io.read(
+                            buffer_size
+                        ).encode("utf-8")
+                # raise Exception()
 
     def store_stream(
         self,
-        stream: list,
+        stream: list[tuple[str, Union[list[tuple], Callable[[], IO]], dict]],
     ):
         """Write the stream to the directory."""
         self._store_stream(self.path, stream)
 
     def _store_stream(self, path, stream):
         for id, data, metadata in stream:
-            if isinstance(data, list):
-                self._store_stream(path / id, data)
-            else:
+            logger.debug(f"data is of type: {type(data)}")
+            if isinstance(data, Callable):
                 self._store_data(path / id, data, metadata)
+            else:
+                self._store_stream(path / id, data)
 
     def retrieve(
         self,
         id: str,
         mode: str = "r",
-    ) -> tuple[Callable[[], Union[TextIO, BinaryIO]], dict]:
+    ) -> tuple[Callable[[], IO], dict]:
         if mode not in ["r", "w", "rb", "wb"]:
             raise Exception("Only 'r', 'w', 'rb', and 'wb' modes are supported.")
         return self._retrieve(self.path / id, mode)
 
     def _retrieve(self, path, mode):
+        if "w" in mode:
+            path.parent.mkdir(parents=True, exist_ok=True)
         return lambda: open(path, mode), {}
 
     def retrieve_stream(
         self,
         selector,
         mode: str = "r",
-    ) -> list[tuple[str, Union[TextIO, BinaryIO, Callable], dict]]:
+    ) -> list[tuple[str, Union[list[tuple], Callable[[], IO]], dict]]:
         """Create a file with the given id as name in the directory."""
 
         if mode not in ["r", "w", "rb", "wb"]:
