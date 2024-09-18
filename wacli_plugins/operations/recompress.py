9b9b3edb0ec326f9cf5145ca1af87e7730fcd07d
Natanael Arndt <arndtn@gmail.com>
2024-09-18 15:23:44 +0200

    Implement RecompressPlugin with warcio StreamRecompressor

--- wacli_plugins/operations/recompress.py
+++ wacli_plugins/operations/recompress.py
@@ -1,9 +1,7 @@
-from pathlib import Path
+from collections.abc import Callable
+from io import BytesIO
 
-from docker.client import from_env as docker_from_env
-from docker.types import Mount
-from loguru import logger
-from warcio.recompressor import Recompressor
+from warcio.recompressor import StreamRecompressor
 
 from wacli.plugin_types import OperationPlugin, StorageStream
 
@@ -17,53 +15,31 @@ class RecompressPlugin(OperationPlugin):
         self.warc_path = configuration.get("warc_path")
 
     def run(self, storage_stream: StorageStream) -> StorageStream:
-        containers = []
-        for warc in storage_stream:
-            warc = self.rebase(
-                Path(warc),
-                base=Path(self.warc_path),
-                to=Path(self.container_path_source),
-            )
-            proc = self.index_warc(warc)
-            containers.append(proc)
-            # ============================================================================
+        return self._iterate_stream(storage_stream)
 
-    def recompress(cmd):
-        _recompressor = Recompressor(cmd.filename, cmd.output, cmd.verbose)
-        _recompressor.recompress()
+    def _iterate_stream(self, storage_stream: StorageStream) -> StorageStream:
+        for id, data, metadata in storage_stream:
+            if isinstance(data, Callable):
+                yield self._recompress(id, data, metadata)
+            else:
+                yield id, self._iterate_stream(data), metadata
 
-    def index_warc(self, warc):
-        env = {"INIT_COLLECTION": self.collection}
-        args = ["wb-manager", "add", self.collection, str(warc)]
-        mounts = [
-            Mount(
-                target=self.container_path_source, source=self.warc_path, type="bind"
-            ),
-            Mount(
-                target=self.container_path_webarchive,
-                source=self.pywb_path,
-                type="bind",
-            ),
-        ]
+    def _recompress(self, id, data, metadata):
+        def data_callback():
+            with data() as source_io:
+                target_io = BytesIO()
+                _recompressor = StreamRecompressor(source_io, target_io, True)
+                if self._is_compressed(id, metadata):
+                    _recompressor.decompress_recompress(data)
+                else:
+                    _recompressor.recompress(data)
 
-        logger.debug(f"command={args}, environment={env}, mounts={mounts},")
+        return id, data_callback, {**metadata, "compression": "application/gzip"}
 
-        client = docker_from_env()
-
-        container = client.containers.run(
-            image="docker.io/webrecorder/pywb",
-            command=args,
-            environment=env,
-            mounts=mounts,
-            detach=True,
-            # auto_remove=True,
-        )
-        logger.debug(container)
-        logger.debug(container.logs())
-        return container
-
-    def rebase(self, warc, base, to):
-        return to / warc.relative_to(base)
+    def _is_compressed(self, id, metadata):
+        if "compression" in metadata:
+            return True
+        return id[:-3] == ".gz"
 
 
-export = PyWbPlugin
+export = RecompressPlugin
