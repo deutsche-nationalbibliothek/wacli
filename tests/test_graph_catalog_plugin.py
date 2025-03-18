33e759c3a505db6ba0050349498a46288edbe7ea
Natanael Arndt <arndtn@gmail.com>
2025-03-18 18:10:08 +0100

    Add test for catalog initialization

--- tests/test_graph_catalog_plugin.py
+++ tests/test_graph_catalog_plugin.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
-
-from loguru import logger
+from unittest.mock import patch
 
 from wacli.plugin_manager import PluginManager
 
@@ -24,15 +23,18 @@ def get_plugin_config(endpoint, path):
     }
 
 
+test_directory = Path(os.path.dirname(__file__))
+
+
 def test_graph_catalog(tmp_path):
     # prepare
     p = tmp_path / "graph_file.ttl"
 
     plugin_manager = PluginManager()
-    plugin_manager.register_plugins(get_plugin_config(None, p))
+    plugin_manager.register_plugins(
+        get_plugin_config("http://no-endpoint.example.org/", p)
+    )
 
-    test_directory = Path(os.path.dirname(__file__))
-    logger.debug(Path(__file__))
     with (
         open(test_directory / "assets/test_websites.ttl", "r") as input,
         open(p, "w") as output,
@@ -41,8 +43,32 @@ def test_graph_catalog(tmp_path):
 
     test_catalog = plugin_manager.get("test_catalog")
 
-    # test_catalog.initialize(example_file, content)
     idn_list = test_catalog.list()
 
     assert "1234567890" in idn_list
     # assert len(list(tmp_path.iterdir())) == 1
+
+
+def test_graph_catalog_initialize(tmp_path):
+    # prepare
+    p = tmp_path / "graph_file.ttl"
+
+    mocked_endpoint = "http://anymocked-sparql-endpoint"
+
+    plugin_manager = PluginManager()
+    plugin_manager.register_plugins(get_plugin_config(mocked_endpoint, p))
+
+    test_catalog = plugin_manager.get("test_catalog")
+
+    with (
+        patch("rdflib.plugins.stores.sparqlconnector.urlopen") as mock_request,
+        open(test_directory / "assets/test_websites.ttl", "rb") as input,
+    ):
+        mock_request.return_value.headers = {"Content-Type": "text/turtle"}
+        mock_request.return_value.read = input.read
+
+        test_catalog.initialize()
+
+    idn_list = test_catalog.list()
+
+    assert "1234567890" in idn_list
