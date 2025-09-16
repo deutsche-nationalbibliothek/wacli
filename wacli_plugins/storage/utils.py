07b18b69438d460b3dab17db05d10866b0c3bcf5
Natanael Arndt <arndtn@gmail.com>
2025-09-16 12:11:42 +0200

    remove not used utils classes

--- wacli_plugins/storage/utils.py
+++ wacli_plugins/storage/utils.py
@@ -1,6 +1,5 @@
 from io import DEFAULT_BUFFER_SIZE, RawIOBase
-from loguru import logger
-from io import DEFAULT_BUFFER_SIZE, TextIOBase, TextIOWrapper
+
 
 class BinaryIOWrapper(RawIOBase):
     """Implement a wrapper to convert a TextIO into a BinaryIO.
@@ -24,37 +23,3 @@ class BinaryIOWrapper(RawIOBase):
 
     def readable(self):
         return True
-
-
-
-class TextToAnyStreamReaderWriter:
-    """A reader and writer that can copy from a TextIO to a TextIO or BinaryIO."""
-    def __init__(self, source_io, target_io):
-        self.source_io, self.target_io = source_io, target_io
-
-    def copy(self):
-        self.source_io.wacli_read = self.source_io.read
-        while chunk := self.source_io.wacli_read(DEFAULT_BUFFER_SIZE):
-            try:
-                self.target_io.write(chunk)
-            except TypeError:
-                logger.debug("overwrite source_io.wacli_read")
-                self.target_io.write(chunk.encode("utf-8"))
-                self.source_io.wacli_read = lambda buffer_size: self.source_io.read(
-                    buffer_size
-                ).encode("utf-8")
-
-
-class UniversalStreamReaderWriter(TextToAnyStreamReaderWriter):
-    """A reader and writer that can copy from """
-    def __init__(self, source_io, target_io):
-        if isinstance(source_io, TextIOBase):
-            mode = "w"
-        else:
-            mode = getattr(source_io, "mode", "wb")
-
-        if "b" not in mode:
-            self.source_io = BinaryIOWrapper(source_io)
-        else:
-            self.source_io = source_io
-        self.target_io = target_io
